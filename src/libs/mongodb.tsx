dada76734510fe509fb9c2e12ce8bd9939eeb4f1
jerome <jerome.studypurpose@gmail.com>
2024-01-30 14:21:07 +0800

    fix

--- src/libs/mongodb.tsx
+++ src/libs/mongodb.tsx
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/nextjs-topic";
 
-
 const connectMongoDB = async () => {
     try {
         mongoose.connect(connectionString);
