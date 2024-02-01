d751079aff68ca4a84f0fdac15e0f70b2425f222
jerome <jerome.studypurpose@gmail.com>
2024-02-02 01:13:46 +0800

    url changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
   if (session) return redirect("/topic")
   return (
     <LoginForm />
